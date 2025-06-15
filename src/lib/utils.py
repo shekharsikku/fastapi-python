c4bcd82822ec26b2836f317aa017e688e23cf18a
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-06-15 10:28:12 +0530

    feat(jose): integrate jose for jwt authentication

--- src/lib/utils.py
+++ src/lib/utils.py
@@ -1,9 +1,10 @@
 from datetime import datetime, timezone, timedelta
 from itsdangerous import URLSafeTimedSerializer
 from passlib.context import CryptContext
+from jose import jwt, JWTError, ExpiredSignatureError
+from typing import Literal
 import logging
 import uuid
-import jwt
 
 from src.config import Config
 
@@ -27,24 +28,34 @@ def verify_password(password: str, hash: str) -> bool:
     return passwd_context.verify(password, hash)
 
 
-def create_access_token(user_data: dict, expiry: timedelta = None, refresh: bool = False):
+def encode_jwt_token(user_data: dict, token_type: Literal["access", "refresh"]):
+    expiration = Config.ACCESS_TOKEN_EXPIRY if token_type == "access" else Config.REFRESH_TOKEN_EXPIRY
     payload = {}
 
     payload["user"] = user_data
-    payload["exp"] = datetime.now() + (expiry if expiry is not None else timedelta(seconds=Config.ACCESS_TOKEN_EXPIRY))
-    payload["jti"] = str(uuid.uuid4())
+    payload["type"] = token_type
+    payload["jti"] = generate_uuid()
+    payload["iat"] = get_timestamp()
+    payload["exp"] = get_timestamp() + timedelta(seconds=expiration)
 
-    payload["refresh"] = refresh
+    encoded_token = jwt.encode(claims=payload, key=Config.JWT_SECRET, algorithm=Config.JWT_ALGORITHM)
+    return encoded_token
 
-    return jwt.encode(payload=payload, key=Config.JWT_SECRET, algorithm=Config.JWT_ALGORITHM)
 
-
-def decode_token(token: str) -> dict:
+def decode_jwt_token(jwt_token: str):
     try:
-        return jwt.decode(jwt=token, key=Config.JWT_SECRET, algorithms=[Config.JWT_ALGORITHM])
-    
-    except jwt.PyJWTError as e:
-        logging.exception(e)
+        decoded_token = jwt.decode(
+            token=jwt_token, 
+            key=Config.JWT_SECRET, 
+            algorithms=Config.JWT_ALGORITHM, 
+            options={"verify_exp": True}
+        )
+        return decoded_token
+    except ExpiredSignatureError:
+        logging.warning("Token has expired.")
+        return None
+    except JWTError as e:
+        logging.exception("Token decoding error.")
         return None
 
 
@@ -58,7 +69,6 @@ def create_url_safe_token(data: dict):
 def decode_url_safe_token(token:str):
     try:
         return serializer.loads(token)
-
     except Exception as e:
         logging.error(str(e))
         return None        

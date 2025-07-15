009971b80242c4c5c51f3a90d862dde96ce08e3c
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-07-15 10:43:16 +0530

    feat(auth): api router and handler for sign-in user

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, Depends, status
 from sqlmodel.ext.asyncio.session import AsyncSession
 
+from src.lib.utils import verify_password, encode_jwt_token, get_timestamp
 from src.lib.response import success_response, error_response
 from src.db.main import get_session
 
-
-from .schemas import UserCreateModel
+from .schemas import UserCreateModel, UserLoginModel
 from .services import UserService
 
 
@@ -23,4 +23,37 @@ async def signup_user(user_data: UserCreateModel, session: AsyncSession = Depend
     await user_service.create_user(user_data, session)
 
     return success_response(status.HTTP_201_CREATED, "Signup successfully!")
+
+
+@auth_router.post("/sign-in")
+async def signin_user(login_data: UserLoginModel, session: AsyncSession = Depends(get_session)):
+    user_exists = (
+        await user_service.get_user_by_email(login_data.email, session)
+        if login_data.email
+        else await user_service.get_user_by_username(login_data.username, session)
+    )
+
+    if not user_exists:
+        return error_response(status.HTTP_404_NOT_FOUND, "User not found!")
+
+    password_valid = verify_password(login_data.password, user_exists.password)
+
+    if not password_valid:
+        return error_response(status.HTTP_401_UNAUTHORIZED, "Invalid credentials!")
+
+    access_token = encode_jwt_token(user_id=user_exists.id, token_type="access")
+    res_data = {
+        "id": user_exists.id, 
+        "email": user_exists.email, 
+        "setup": user_exists.setup, 
+        "access_token": access_token
+    }
+
+    if not user_exists.setup:
+        return success_response(status.HTTP_200_OK, "Please, complete your profile!", res_data)
+    
+    refresh_token = encode_jwt_token(user_data=user_exists.id, token_type="refresh")
+    res_data["refresh_token"] = refresh_token
+
+    return success_response(status.HTTP_200_OK, "Signin successfully!", res_data)
 

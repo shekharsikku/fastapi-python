d2f9bc77899ac222b899c2f4b8f80d924bace0cc
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-08-22 12:15:32 +0530

    feat(auth): logic for cache user data and refresh tokens

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -5,6 +5,7 @@ from datetime import datetime, timezone
 from src.lib.response import SuccessResponse, ErrorResponse
 from src.lib.utils import verify_password, encode_jwt_token, get_timestamp
 from src.lib.dependencies import get_current_user, refresh_token_bearer
+from src.db.redis import redis_set_json, redis_set_string
 from src.db.main import get_session
 
 from .schemas import UserSignupModel, UserSigninModel, UserModel
@@ -43,32 +44,53 @@ async def signin_user(login_data: UserSigninModel, session: AsyncSession = Depen
     if not password_valid:
         raise ErrorResponse(status=status.HTTP_401_UNAUTHORIZED, message="Invalid credentials!")
 
+    user_data = UserModel.model_validate(user_exists, from_attributes=True).model_dump(mode="json")
+    user_data_result = await redis_set_json(f"user:{user_exists.id}", user_data)
+
+    if not user_data_result:
+        raise ErrorResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR, message="Unable to signin!")
+
     access_token = encode_jwt_token(user_id=user_exists.id, token_type="access")
-    res_data = {
-        "id": user_exists.id, 
-        "email": user_exists.email, 
-        "setup": user_exists.setup, 
-        "access_token": access_token
-    }
+    res_data = {"user": user_data, "token": {"access": access_token}}
 
     if not user_exists.setup:
-        return SuccessResponse(status=status.HTTP_200_OK, message="Please, complete your profile!", data=res_data)
+        return SuccessResponse(status=status.HTTP_200_OK, message="Please, update your profile!", data=res_data)
     
     refresh_token = encode_jwt_token(user_id=user_exists.id, token_type="refresh")
-    res_data["refresh_token"] = refresh_token
+    refresh_token_result = await redis_set_string(f"refresh:{user_exists.id}", refresh_token, 86400)
+
+    if not refresh_token_result:
+        raise ErrorResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR, message="Unable to signin!")
+
+    res_data["token"]["refresh"] = refresh_token
 
     return SuccessResponse(status=status.HTTP_200_OK, message="Signin successfully!", data=res_data)
 
 
 @auth_router.get("/me")
-async def get_user_info(query_user=Depends(get_current_user)):
-    user_data = UserModel.model_validate(query_user, from_attributes=True).model_dump(mode="json")
-    return SuccessResponse(status=status.HTTP_200_OK, message="User information!", data=user_data)
+async def get_user_info(user_data=Depends(get_current_user)):
+    if user_data is not None:
+        return SuccessResponse(status=status.HTTP_200_OK, message="User information!", data=user_data)
+    raise ErrorResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR, message="Something went wrong!")
 
 
 @auth_router.get("/refresh-token")
-async def get_new_access_token(token_data: dict = Depends(refresh_token_bearer)):
+async def get_new_access_token(token_data: dict = Depends(refresh_token_bearer), session: AsyncSession = Depends(get_session)):
     if datetime.fromtimestamp(token_data["exp"], timezone.utc) > get_timestamp():
-        access_token = encode_jwt_token(user_id=token_data["uid"], token_type="access")
-        return SuccessResponse(status=status.HTTP_200_OK, message="Token refreshed successfully!", data=access_token)
-    raise ErrorResponse(status=status.HTTP_401_UNAUTHORIZED, message="Token is invalid or expired!")
+        current_user_id = token_data["uid"]
+
+        query_data = await user_service.get_user_by_id(current_user_id, session)
+        user_data = UserModel.model_validate(query_data, from_attributes=True).model_dump(mode="json")
+        user_data_result = await redis_set_json(f"user:{current_user_id}", user_data)
+
+        access_token = encode_jwt_token(user_id=current_user_id, token_type="access")
+        refresh_token = encode_jwt_token(user_id=current_user_id, token_type="refresh")
+        refresh_token_result = await redis_set_string(f"refresh:{current_user_id}", refresh_token, 86400)
+
+        if not refresh_token_result or not user_data_result:
+            raise ErrorResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR, message="Unable to refresh!")
+
+        res_data = {"user": user_data, "token": {"access": access_token, "refresh": refresh_token}}
+        return SuccessResponse(status=status.HTTP_200_OK, message="New tokens issued successfully!", data=res_data)
+    
+    raise ErrorResponse(status=status.HTTP_401_UNAUTHORIZED, message="Refresh token is expired!")
--- src/lib/dependencies.py
+++ src/lib/dependencies.py
@@ -6,8 +6,10 @@ from typing import Literal
 
 from src.lib.response import ErrorResponse
 from src.lib.utils import decode_jwt_token
+from src.db.redis import redis_set_json, redis_get_json, redis_get_string
 from src.db.main import get_session
 from src.auth.services import UserService
+from src.auth.schemas import UserModel
 
 
 user_service = UserService()
@@ -32,6 +34,12 @@ class TokenBearer(HTTPBearer):
             if self.auto_error:
                 raise ErrorResponse(status=status.HTTP_401_UNAUTHORIZED, message=f"Please, provide a valid {self.token_type} token!")
             return None
+        
+        if self.token_type == "refresh":
+            stored_token = await redis_get_string(f"refresh:{token_data["uid"]}")
+
+            if stored_token is None or stored_token != auth_token:
+                raise ErrorResponse(status=status.HTTP_401_UNAUTHORIZED, message="Refresh token is invalid!")
 
         return token_data
         
@@ -41,4 +49,13 @@ refresh_token_bearer = TokenBearer(token_type="refresh")
 
 
 async def get_current_user(token_data: dict = Depends(access_token_bearer), session: AsyncSession = Depends(get_session)):
-    return await user_service.get_user_by_id(token_data["uid"], session)
+    cache_data = await redis_get_json(f"user:{token_data["uid"]}")
+
+    if cache_data is not None:
+        return cache_data
+    
+    query_data = await user_service.get_user_by_id(token_data["uid"], session)
+    user_data = UserModel.model_validate(query_data, from_attributes=True).model_dump(mode="json")
+    user_data_result = await redis_set_json(f"user:{token_data["uid"]}", user_data)
+
+    return user_data if user_data_result else None

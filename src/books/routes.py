59ac28a3b3461633fe024fef0c46aa87dd0f7931
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-08-31 11:40:10 +0530

    refactor(response): change structure of books data to json

--- src/books/routes.py
+++ src/books/routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, status
 from sqlmodel.ext.asyncio.session import AsyncSession
+from pydantic import TypeAdapter
 from typing import List
 
 from src.books.schemas import BookModel, BookCreateModel
@@ -11,27 +12,37 @@ from src.lib.dependencies import access_token_bearer
 
 book_router = APIRouter()
 book_service = BookService()
+book_adapter = TypeAdapter(List[BookModel])
 
 
-@book_router.post("/", status_code=status.HTTP_201_CREATED, response_model=BookModel)
+@book_router.post("/")
 async def create_a_book(book_data: BookCreateModel, token_data: dict = Depends(access_token_bearer), session: AsyncSession = Depends(get_session)):
-    return await book_service.create_book(book_data, token_data["uid"], session)
+    current_book = await book_service.create_book(book_data, token_data["uid"], session)
+    book_data = BookModel.model_validate(current_book, from_attributes=True).model_dump(mode="json")
+    return SuccessResponse(status=status.HTTP_201_CREATED, message="Book created successfully!", data=book_data)
 
 
-@book_router.get("/", response_model=List[BookModel])
+@book_router.get("/")
 async def get_all_books(session: AsyncSession = Depends(get_session)):
-    return await book_service.get_all_books(session)
+    all_books = await book_service.get_all_books(session)
+    books_data = book_adapter.validate_python(all_books, from_attributes=True)
+    books_data = book_adapter.dump_python(books_data, mode="json")
+    return SuccessResponse(status=status.HTTP_200_OK, message="Books fetched successfully!", data=books_data)
 
 
-@book_router.get("/{book_id}", response_model=BookModel)
+@book_router.get("/{book_id}")
 async def get_book(book_id: int, session: AsyncSession = Depends(get_session)):
     current_book = await book_service.get_book(book_id, session)
     if current_book:
-        return current_book
+        book_data = BookModel.model_validate(current_book, from_attributes=True).model_dump(mode="json")
+        return SuccessResponse(status=status.HTTP_200_OK, message="Book fetched successfully!", data=book_data)
     else:
         raise ErrorResponse(status=status.HTTP_404_NOT_FOUND, message="Book not found!")
 
 
-@book_router.get("/user/{user_id}", response_model=List[BookModel])
+@book_router.get("/user/{user_id}")
 async def get_user_books(user_id: int, session: AsyncSession = Depends(get_session)):
-    return await book_service.get_user_books(user_id, session)
+    all_books = await book_service.get_user_books(user_id, session)
+    books_data = book_adapter.validate_python(all_books, from_attributes=True)
+    books_data = book_adapter.dump_python(books_data, mode="json")
+    return SuccessResponse(status=status.HTTP_200_OK, message="Books fetched successfully!", data=books_data)
